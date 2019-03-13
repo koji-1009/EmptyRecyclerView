9aa3f8c438cf7a3cd70bc4fd5d631694919821d3
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-03-13 09:33:18 +0900

    Set default LayoutParams when the params is null for Android 5

--- emptyrecyclerview/src/main/java/com/app/dr1009/emptyrecyclerview/EmptyRecyclerView.java
+++ emptyrecyclerview/src/main/java/com/app/dr1009/emptyrecyclerview/EmptyRecyclerView.java
@@ -190,6 +190,8 @@ public class EmptyRecyclerView extends RecyclerView {
         textView.setGravity(gravity);
         if (params != null) {
             textView.setLayoutParams(params);
+        } else {
+            textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         }
 
         mEmptyView = textView;
